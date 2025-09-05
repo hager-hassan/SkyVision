278068727367b108d82db678bc569e9ae16f0606
Hager Hassan <hassanhager830@gmail.com>
2025-09-05 21:46:07 +0300

    enhance HourDataCard style

--- src/Components/HourDataCard/HourDataCard.tsx
+++ src/Components/HourDataCard/HourDataCard.tsx
@@ -8,15 +8,15 @@ interface HourDataCardProps {
 export default function HourDataCard({ hourData }: HourDataCardProps) {
   return (
     <div className="bg-white dark:bg-gray-700 p-3 rounded-xl border-2 border-gray-100 flex items-center justify-center transition-all duration-300">
-      <div className="flex flex-wrap gap-1">
-        <div>
-          <img src={hourData.icon} alt="weather icon" className="text-center" />
+      <div className="flex flex-nowrap gap-1">
+        <div className='flex items-center shrink-0'>
+          <img src={hourData.icon} alt="weather icon" />
         </div>
 
-        <div className="font-semibold flex flex-col gap-1 justify-center">
+        <div className="font-semibold flex flex-col gap-1 justify-center shrink">
             <span className="text-gray-500 dark:text-white text-xs transition-all duration-300">{hourData.time}</span>
             <span className="text-gray-500 dark:text-white text-xs transition-all duration-300">{hourData.temperature}<sup>°</sup>C</span>
-            <span className="text-gray-600 dark:text-white text-sm transition-all duration-300">{hourData.condition}</span>
+            <span className="text-gray-600 dark:text-white text-xs transition-all duration-300 sm:text-sm">{hourData.condition}</span>
         </div>
       </div>
     </div>
